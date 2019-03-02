20fac4d39d144ffd9ae89ce2582f84cc439d40e6
Dario N <losman1987naj.@gmail.com>
2019-03-02 15:00:55 +0100

    pierwszy commit

--- src/OperationValidator.java
+++ src/OperationValidator.java
@@ -1,5 +1,8 @@
 public class OperationValidator {
     public static void validate(String działanie) {
-        throw new NoSuchMethodError();
+        if (działanie.trim().length() > 1 && !działanie.trim().matches("/[+-/*]/g"))
+            throw new UnsupportedOperationException("To nie jest znak działania matematycznego");
     }
+
 }
+
